bcc331d30e879f20477c3776b7853d27b9fa9193
michellejun <mj15@illinois.edu>
2018-11-28 14:30:37 -0600

    Palette and GitHub

--- app/src/main/java/com/example/michellejun/csfinalapp/colorGame.java
+++ app/src/main/java/com/example/michellejun/csfinalapp/colorGame.java
@@ -1,7 +1,9 @@
 package com.example.michellejun.csfinalapp;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 
 public class colorGame extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class colorGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_game);
+    }
+    public Palette createPaletteSynch(Bitmap bitmap) {
+        Palette p = Palette.from(bitmap).generate();
+        return p;
     }
 }
