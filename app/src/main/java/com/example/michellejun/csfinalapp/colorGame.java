95d66d4840dad965cd34ee498ea47a3937e4bf3a
michellejun <mj15@illinois.edu>
2018-11-28 13:25:06 -0600

    Initial commit

---
+++ app/src/main/java/com/example/michellejun/csfinalapp/colorGame.java
@@ -0,0 +1,13 @@
+package com.example.michellejun.csfinalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class colorGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_game);
+    }
+}
